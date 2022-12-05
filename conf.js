d9376f7d443a469d04f61a45b59f9cf6ad3eec74
Journel Cabrillos <65072908+prothej227@users.noreply.github.com>
2022-12-06 00:51:23 +0800

    added icons

---
+++ conf.js
@@ -0,0 +1,15 @@
+var config = {
+
+loginvc : "Enter the voucher code then click CONNECT.",
+loginup : "Enter the username and password <br> then click CONNECT.",
+voucherCode : "Code Voucher",
+setCase : "none", // lowercase, uppercase or none
+defaultMode : "voucher", // voucher or member
+theme : "default", // default, dark, lite
+
+// status expire
+
+url : "jrnl.swiftlink", // url server Mikhmon
+SessionName : "swiftlink", 
+
+}
